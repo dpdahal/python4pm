c84bd3a2cbb9ce409ff5bc5ab370528acc66e35f
dpdahal <dp48dahal@gmail.com>
2024-03-15 16:39:45 +0545

    update

---
+++ introduction.py
@@ -0,0 +1,84 @@
+# print("Hello world")
+# print(34567)
+# print("ram")
+
+# name ='ram'
+# print(name)
+
+# print('sita')
+
+
+# x=5
+# y=10
+
+# print(x)
+# print(y)
+
+# x,y = 5,10
+# print(x,y)
+# print(y)
+
+# *a,b,c=56,78,89,67,54,34,56,665,567
+# print(a)
+# print(b)
+# print(c)
+
+# num =(3+4) *9 + 5
+# print(num)
+
+
+# a =5
+# a=300
+# print(a)
+
+# num =input("Enter a number: ")
+# print(num)
+
+# x=int(input("Enter  x: "))
+# y =int(input("Enter  y: "))
+# print("total number is ",x+y)
+
+# p=1000
+# r=10
+# t=2
+# si = (p*r*t)/100
+# print("Simple interest is ",si)
+
+
+# f_name ='ram'
+# l_name ='sharma'
+
+# print(f_name + l_name)
+
+# x=5
+# y=10
+# print(x+y)
+
+
+# print("hello"+40)
+
+
+x=50
+y=10
+
+# print(x<y)
+
+# if x>y:
+#     print("x is greater than y")
+
+# else:
+#     print("y is greater than x")
+
+
+# print("Hello world")
+
+# for x in range(11):
+#     print("Hello world")
+
+
+
+# print("Hello world")
+
+
+
+print("data")
